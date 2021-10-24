7337e68188a03856d1720beae75087d96aca3ad5
Ian G <yo.ian.g@gmail.com>
2021-10-23 22:24:19 -0400

    IRandom interface, SystemRandomWrapper

--- Runtime/IEnumerableExtensions/Random.cs
+++ Runtime/IEnumerableExtensions/Random.cs
@@ -7,35 +7,37 @@ namespace Eunomia
     public static partial class IEnumerableExtensions
     {
         // TODO: use IRandom interface
-        private static System.Random fallbackRandom = null;
+        private static IRandom fallbackRandom = null;
 
-        private static System.Random GetFallbackRandom()
+        private static IRandom GetFallbackRandom()
         {
             if (fallbackRandom != null)
             {
                 return fallbackRandom;
             }
 
-            fallbackRandom = new System.Random((int)(new DateTime()).TimeOfDay.TotalMilliseconds);
+            fallbackRandom = new SystemRandomWrapper(
+                new System.Random((int)(new DateTime()).TimeOfDay.TotalMilliseconds)
+            );
             return fallbackRandom;
         }
 
         /// <summary>
-        /// 
+        /// Return a randomly selected index within `enumerable`
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="enumerable"></param>
         /// <param name="random"></param>
-        /// <returns></returns>
-        /// <exception cref="System.IndexOutOfRangeException">If the enumerable has 0 elements in it</exception>
-        public static int RandomIndex<T>(this IEnumerable<T> enumerable, System.Random random = null)
+        /// <returns>A randomly selected index within `enumerable`</returns>
+        /// <exception cref="System.ArgumentException">The enumerable has 0 elements in it</exception>
+        public static int RandomIndex<T>(this IEnumerable<T> enumerable, IRandom random = null)
         {
             if (enumerable.Count() <= 0)
             {
-                throw new IndexOutOfRangeException("Provided IEnumerable with 0 elements");
+                throw new ArgumentException("Provided IEnumerable with 0 elements", "enumerable");
             }
 
-            System.Random useRandom;
+            IRandom useRandom;
             if (random == null)
             {
                 useRandom = GetFallbackRandom();
@@ -49,18 +51,18 @@ namespace Eunomia
         }
 
         /// <summary>
-        /// Return a randomly selected element within our enumerable
+        /// Return a randomly selected element within `enumerable`
         /// </summary>
         /// <param name="enumerable">Enumerable to return an element from</param>
         /// <param name="random">Optional System.Random random instance</param>
-        /// <returns>Randomly selected element from within our enumerable</returns>
-        /// <exception cref="System.IndexOutOfRangeException">If the enumerable has 0 elements in it</exception>
-        /// <exception cref="System.InvalidOperationException">If internally used Eunomia.IEnumerableExtensions_Linq_Naming.RandomIndex method returns an index out of range of our enumerable</exception>
-        public static T RandomElement<T>(this IEnumerable<T> enumerable, System.Random random = null)
+        /// <returns>A randomly selected element from within `enumerable`</returns>
+        /// <exception cref="System.ArgumentException">The enumerable has 0 elements in it</exception>
+        /// <exception cref="System.InvalidOperationException">Internally used Eunomia.IEnumerableExtensions.RandomIndex method returns an index out of range of our enumerable</exception>
+        public static T RandomElement<T>(this IEnumerable<T> enumerable, IRandom random = null)
         {
             if (enumerable.Count() <= 0)
             {
-                throw new IndexOutOfRangeException("Provided IEnumerable with 0 elements");
+                throw new ArgumentException("Provided IEnumerable with 0 elements", "enumerable");
             }
 
             var index = RandomIndex(enumerable, random);
---
+++ Runtime/Random/IRandom.cs
@@ -0,0 +1,27 @@
+namespace Eunomia
+{
+    public interface IRandom
+    {
+        /// <summary>
+        /// Returns a non-negative random integer.
+        /// </summary>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than int.MaxValue.</returns>
+        int Next();
+
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum.
+        /// </summary>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. `maxValue` must be greater than or equal to 0.</param>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0, and less than `maxValue`; that is, the range of return values ordinarily includes 0 but not `maxValue`. However, if `maxValue` equals 0, maxValue is returned.</returns>
+        int Next(int maxValue);
+
+        /// <summary>
+        /// Returns a random integer that is within a specified range.
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The exclusive upper bound of the random number returned. `maxValue` must be greater than or equal to `minValue`.</param>
+        /// <returns>A signed integer greater than or equal to `minValue` and less than `maxValue`; that is, the range of return values includes `minValue` but not `maxValue`. If `minValue` equals `maxValue`, `minValue` is returned.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">`minValue` is greater than `maxValue`.</exception>
+        int Next(int minValue, int maxValue);
+    }
+}
